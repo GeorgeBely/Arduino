45a021d5c545e1e1c15e218002b260a1124275ac
georg <man240893@mail.ru>
2015-02-06 09:34:55 +0300

    Microphone replace

--- Lesson/L18_Microphone/L18_Microphone.ino
+++ Lesson/L18_Microphone/L18_Microphone.ino
@@ -4,6 +4,8 @@ const int microphonePin = A3;
 //Пороговая величина (Хлопок)
 const int thresholdValue = 400; 
 
+boolean ledValue = false;
+
 void setup() { 
   pinMode(ledPin, OUTPUT);  
 } 
@@ -12,10 +14,9 @@ void loop() {
   //Считываем данные с микро
   int sensorValue = analogRead(microphonePin); 
   
-  //Если уровень шума больше порогового значения включаем светодиод
-  if (sensorValue > thresholdValue) {
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-  }
+  //Если уровень шума больше порогового значения меняем значение светодиода
+  if (sensorValue > thresholdValue) 
+    ledValue = !ledValue;
+    
+  digitalWrite(ledPin, ledValue);
 }
