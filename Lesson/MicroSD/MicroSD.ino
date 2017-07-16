b8cb68a99ca1b5a066b5fb9354866481095e2508
gbeliy <man240893@mail.ru>
2017-07-16 08:30:56 +0300

    Update MicroSD sketch

--- Lesson/MicroSD/MicroSD.ino
+++ Lesson/MicroSD/MicroSD.ino
@@ -1,67 +1,65 @@
 #include <SD.h>
 
-//https://arduino-info.wikispaces.com/SD-Cards
-
-File myFile;
 
 void setup() {
-
     Serial.begin(9600);
-
     Serial.print("Initializing SD card...");
 
-// на Ethernet шилде CS соответствует 4 пину. По умолчанию он установлен в режим output
-
-// обратите внимание, что если он не используется в качестве CS пина, SS пин на оборудовании
-
-// (10 на большинстве плат Arduino, 53 на Arduino Mega) надо оставить в режиме output.
-
-// иначе функции библиотеки SD library не будут работать.
-
+    // на Ethernet шилде CS соответствует 4 пину. По умолчанию он установлен в режим output
+    // обратите внимание, что если он не используется в качестве CS пина, SS пин на оборудовании
+    // (10 на большинстве плат Arduino, 53 на Arduino Mega) надо оставить в режиме output.
+    // иначе функции библиотеки SD library не будут работать.
     pinMode(10, OUTPUT);
 
     if (!SD.begin(10)) {
-
         Serial.println("initialization failed!");
-
         return;
-
     }
-
     Serial.println("initialization done.");
-
-// открываем файл. Обратите внимание, что открывать несколько файлов параллельно нельзя.
-
-// перед открытием нового файла, старый надо закрыть
-
-    myFile = SD.open("test.txt", FILE_WRITE);
-
-// если удалось открыть файл, записываем в него:
-
-    if (myFile) {
-
-        Serial.print("Writing to test.txt...");
-
-        myFile.println("testing ololo pish pish");
-
-// закрываем файл:
-
-        myFile.close();
-
-        Serial.println("done.");
-
-    } else {
-
-// если файл не открылся, выводим сообщение об ошибке:
-
-        Serial.println("error opening test.txt");
-
-    }
-
 }
 
 void loop() {
+    for (int i = 0; i < 500; i++) {
+        writeToFile("test.txt", i);
+        writeToFile("test2.txt", i + 1);
+        delay(500);
+    }
+}
 
-// после setup ничего не происходит
 
+bool writeToFile(String fileName, String text) {
+    // открываем файл. Обратите внимание, что открывать несколько файлов параллельно нельзя.
+    // перед открытием нового файла, старый надо закрыть
+    File myFile = SD.open(fileName, FILE_WRITE);
+
+    // если удалось открыть файл, записываем в него:
+    if (myFile) {
+        Serial.print("Writing to ");
+        Serial.print(fileName);
+        Serial.println("...");
+
+        myFile.println(text);
+
+        // закрываем файл:
+        myFile.close();
+
+        Serial.print("Close file: ");
+        Serial.println(fileName);
+
+        return true;
+    } else {
+        // если файл не открылся, выводим сообщение об ошибке:
+        Serial.print("error opening ");
+        Serial.println(fileName);
+
+        return false;
+    }
+}
+
+
+String convertToStr(int value) {
+    char sCount[5];
+    itoa(value, sCount, 10);
+
+    return sCount;
 }
