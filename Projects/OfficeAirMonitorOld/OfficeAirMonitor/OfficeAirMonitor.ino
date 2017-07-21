22b12614a0ab175187bb8c47edaa0c3736151958
beliy <man240893@mail.ru>
2017-07-21 17:47:43 +0300

    update office air monitor

--- Projects/OfficeAirMonitorOld/OfficeAirMonitor/OfficeAirMonitor.ino
+++ Projects/OfficeAirMonitorOld/OfficeAirMonitor/OfficeAirMonitor.ino
@@ -31,16 +31,15 @@
 #define ATMOCO2 397.13
 
 
-#define RZERO_CALIBRATING_MASS_SIZE 1
-#define RZERO_CALIBRATING_COUNT 1
+//#define RZERO_CALIBRATING_MASS_SIZE 50
+//#define RZERO_CALIBRATING_COUNT 5
 
 
-#define CYCLE_ITERATION_COUNT 1
+#define CYCLE_ITERATION_COUNT 30
 
 /** С помощью этой переменной будем получать и обрабатывать данные с термометра */
 DHT dht(DHT_PIN, DHTTYPE);
 
-//MQ135 mq(CO2_PIN);
 
 /**
  * С помощью этой переменной будем отрисовывать данные на дисплей
@@ -52,14 +51,18 @@ LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 int count = 0;
 float humidityMass[CYCLE_ITERATION_COUNT];
 float temperatureMass[CYCLE_ITERATION_COUNT];
+float rZeroCO2Mass[CYCLE_ITERATION_COUNT];
 double co2Mass[CYCLE_ITERATION_COUNT];
+int analogCO2Mass[CYCLE_ITERATION_COUNT];
 
 float lastCorrectHumidity = 0;
 float lastCorrectTemperature = 0;
 double lastCorrectCo2 = 0;
+int lastCorrectAnalogCO2 = 0;
+float lastCorrectRZero = 0;
 
-int hr = 18;
-int mn = 45;
+int hr = 12;
+int mn = 53;
 int sk = 30;
 
 
@@ -85,9 +88,9 @@ void loop() {
     float h = dht.readHumidity(); // Считываем влажность воздуха в процентах
     float t = dht.readTemperature(); // Считываем температуру в градусах
 
-  //  updateRZero(t, h);
     int a = analogRead(A0);
-    double CO2Tmp = getCorrectedPPM(a, h, t);
+    double CO2Tmp = getCorrectedPPM(a, t, h);
+    float z = getCorrectedRZero(a, t, h);
 
     // проверяем, что данные корректны
     if (!isnan(t)) {
@@ -105,19 +108,35 @@ void loop() {
     } else {
         Serial.println("Failed to read from CO2 sensor");
     }
+    if (a > 10) {
+        lastCorrectAnalogCO2 = a;
+    } else {
+        Serial.println("Failed to read analog CO2 sensor");
+    }
+    if (z > 10) {
+        lastCorrectRZero = z;
+    } else {
+        Serial.println("Failed to read from rZero");
+    }
 
     if (count >= CYCLE_ITERATION_COUNT) {
         float temperature = 0;
         float humidity = 0;
         double co2 = 0;
+        int analog = 0;
+        float rzero = 0;
         for (int i = 0; i < CYCLE_ITERATION_COUNT; i++) {
             temperature += temperatureMass[i];
             humidity += humidityMass[i];
             co2 += co2Mass[i];
+            analog += analogCO2Mass[i];
+            rzero += rZeroCO2Mass[i];
         }
         temperature = temperature/CYCLE_ITERATION_COUNT;
         humidity = humidity/CYCLE_ITERATION_COUNT;
         co2 = co2/CYCLE_ITERATION_COUNT;
+        analog = analog/CYCLE_ITERATION_COUNT;
+        rzero = rzero/CYCLE_ITERATION_COUNT;
 
 
         Serial.print("time: ");
@@ -127,18 +146,24 @@ void loop() {
         Serial.print(":");
         Serial.print(sk);
         Serial.print("\t");
+
         Serial.print("RZERO: ");
-        Serial.print(getCorrectedRZero(a, t, h));
+        Serial.print(rzero);
         Serial.print("\t");
+
+        Serial.print("A: ");
+        Serial.print(analog);
+        Serial.print("\t");
+
         Serial.print("Humidity: ");
         Serial.print(humidity);
         Serial.print(" %\t");
+
         Serial.print("Temperature: ");
         Serial.print(temperature);
         Serial.print(" *C\t");
-        Serial.print("A: ");
-        Serial.print(a);
-        Serial.print("\tCO2: ");
+
+        Serial.print("CO2: ");
         Serial.print(co2);
         Serial.println(" ppm");
 
@@ -156,12 +181,15 @@ void loop() {
     } else {
         temperatureMass[count] = lastCorrectTemperature;
         humidityMass[count] = lastCorrectHumidity;
+        rZeroCO2Mass[count] = lastCorrectRZero;
         co2Mass[count] = lastCorrectCo2;
+        analogCO2Mass[count] = lastCorrectAnalogCO2;
         count++;
     }
 
     updateTime();
-    delay(1000);
+ //   updateRZero(a, t, h);
+    delay(880);
 }
 
 /**
@@ -189,11 +217,11 @@ String convertToStr(double value) {
 
 
 /// Calibration resistance at atmospheric CO2 level
-float rZero = 75.00;
-float rZeroMass[RZERO_CALIBRATING_MASS_SIZE];;
-float rZeroEndMass[RZERO_CALIBRATING_COUNT];
-int rZeroCount = 0;
-int rZeroEndCount = 0;
+float rZero = 65.00;
+//float rZeroMass[RZERO_CALIBRATING_MASS_SIZE];;
+//float rZeroEndMass[RZERO_CALIBRATING_COUNT];
+//int rZeroCount = 0;
+//int rZeroEndCount = 0;
 
 float getCorrectionFactor(float t, float h) {
     return CORA * t * t - CORB * t + CORC - (h-33.)*CORD;
@@ -224,30 +252,33 @@ float getCorrectedRZero(int val, float t, float h) {
 }
 
 
-void updateRZero(float t, float h) {
-  /*  if (rZeroCount >= RZERO_CALIBRATING_MASS_SIZE) {
-        float summ = 0.0;
-        for (int i = 0; i < RZERO_CALIBRATING_MASS_SIZE; i++) {
-            summ += rZeroMass[i];
-        }
-        if (rZeroEndCount >= RZERO_CALIBRATING_COUNT) {
-            float summEnd = 0.0;
-            for (int i = 0; i < RZERO_CALIBRATING_COUNT; i++) {
-                summEnd += rZeroEndMass[i];
-            }
-            rZero = summEnd/rZeroEndCount;
-            Serial.print("rZero: ");
-            Serial.println(rZero);
-            rZeroEndCount = 0;
-        } else {
-            rZeroEndMass[rZeroEndCount] = summ/rZeroCount;
-            rZeroEndCount++;
-        }
-        rZeroCount = 0;
-    } else {
-        rZeroMass[rZeroCount] = getCorrectedRZero(t, h);
-        rZeroCount++;
-    }*/
+void updateRZero(int val, float t, float h) {
+//    if (hr != 3) {
+//      return;
+//    }
+//    if (rZeroCount >= RZERO_CALIBRATING_MASS_SIZE) {
+//        float summ = 0.0;
+//        for (int i = 0; i < RZERO_CALIBRATING_MASS_SIZE; i++) {
+//            summ += rZeroMass[i];
+//        }
+//        if (rZeroEndCount >= RZERO_CALIBRATING_COUNT) {
+//            float summEnd = 0.0;
+//            for (int i = 0; i < RZERO_CALIBRATING_COUNT; i++) {
+//                summEnd += rZeroEndMass[i];
+//            }
+//            rZero = summEnd/rZeroEndCount;
+//            Serial.print("rZero: ");
+//            Serial.println(rZero);
+//            rZeroEndCount = 0;
+//        } else {
+//            rZeroEndMass[rZeroEndCount] = summ/rZeroCount;
+//            rZeroEndCount++;
+//        }
+//        rZeroCount = 0;
+//    } else {
+//        rZeroMass[rZeroCount] = getCorrectedRZero(val, t, h);
+//        rZeroCount++;
+//    }
 }
 
 void updateTime() {
@@ -264,6 +295,8 @@ void updateTime() {
       hr = 0;
     }
 }
+
+
 
 
 
